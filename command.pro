6ee3fb19c6c53d1bc6ffd329d08fe7cc13d127df
tamarRotShtein <tamar145290@gmail.com>
2022-08-01 14:26:22 +0300

    second commit

--- command.pro
+++ command.pro
@@ -8,7 +8,7 @@ CONFIG -= app_bundle
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        command_api.cpp \
+        command_api.c \
         main.cpp
 
 # Default rules for deployment.
@@ -17,6 +17,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 HEADERS += \
+    Queue.h \
     command_api.h \
-    gaz_cam.h \
-    queue.h
+    gas_cam.h
--- main.cpp
+++ main.cpp
@@ -1,28 +1,48 @@
 #include <QCoreApplication>
 #include "command_api.h"
 #include <dlfcn.h>
-#include "gaz_cam.h"
+#include "gas_cam.h"
 #include <unistd.h>
+#include <signal.h>
 
+//insert to the command.c
 
+static handler* my_handler;
+static gazapi_t *p_gaz;
+int is_active =1;
+void sig_handler(int sig_no)
+{
+    printf("sigint recieved");
+    p_gaz->stop_record(my_handler);
+  is_active=0;
+    exit(0);
+}
 int main(int argc, char *argv[])
-{   
-   //exec_command_line();
+{
+    //signal(SIGINT, sig_handler);
+    printf("kkkkkk");
+    //exec_command_line();
+    //function to load library
     void* libHandle= dlopen("/home/tamar/amazon_project/final/build-gaz_cam_api-Desktop-Debug/libgaz_cam_api.so.1.0.0",RTLD_LAZY);
     assert (libHandle);
-    gazapi_t *p_gaz;
+
     p_gaz = (gazapi_t*)dlsym(libHandle,"p_gaz_api");
     assert(p_gaz);
-    void* handle=p_gaz->init();
-    handler* h=(handler*)(handle);
+    //needs to be global for all
+    void* handle=p_gaz->init();   //in startrecordcli
+    my_handler=(handler*)(handle);
+    //-------------------------------
     p_gaz->start_record(handle);
-    while(1)
+    //catch the sigint
+
+
+    while(is_active)
     {
-        printf("n\n");
-        sleep(20);
+        //printf("n\n");
+        sleep(1);
     }
-    QCoreApplication a(argc, argv);
-    return a.exec();
+   // QCoreApplication a(argc, argv);
+    //return a.exec();
 }
 
 
